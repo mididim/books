da6d123d315b94deb20d2397acc431d09a6395c4
Dimitry <mididim@gmail.com>
2022-01-08 12:54:09 +0200

    ready

--- src/test/java/lv/sda/books/ApplicationTest.java
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -7,30 +7,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ApplicationTest {
 
     @Test
-    public void sampleTest_0(){
+    public void searchBook(){
         Bookstore BS = new Bookstore();
-        assertEquals(12,BS.searchBook("", "searchAllBooks").size());
+        assertEquals(5,BS.searchBook("", "searchAllBooks").size());
     }
 
     @Test
-    public void sampleTest_1(){
+    public void addBook(){
         Bookstore BS = new Bookstore();
         Book newBook = new Book("00001", "Title", "Description", "Author", "Publisher", 100, LocalDate.of(2000,1,1));
         BS.addBook(newBook);
-        assertEquals(13,BS.searchBook("", "searchAllBooks").size());
+        assertEquals(6,BS.searchBook("", "searchAllBooks").size());
     }
 
     @Test
-    public void sampleTest_2(){
+    public void searchByTitle(){
         Bookstore BS = new Bookstore();
-        assertEquals(3,BS.searchBook("Book 222", "searchByTitle").size());
+        assertEquals(0,BS.searchBook("Book 222", "searchByTitle").size());
     }
 
     @Test
-    public void sampleTest_3(){
+    public void removeBook(){
         Bookstore BS = new Bookstore();
-        BS.removeBook(BS.searchBook("123456", "searchByIsbn").get(0));
-        assertEquals(11,BS.searchBook("", "searchAllBooks").size());
+        BS.removeBook(BS.searchBook("12324567894", "searchByIsbn").get(0));
+        assertEquals(4,BS.searchBook("", "searchAllBooks").size());
     }
 
 }
